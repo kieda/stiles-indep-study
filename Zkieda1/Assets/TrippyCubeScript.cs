91284932698cd200a31cc7931bfdf810825bb889
kieda <zakkieda@gmail.com>
2016-05-12 08:39:10 -0400

    updated trippy cube

--- Zkieda1/Assets/TrippyCubeScript.cs
+++ Zkieda1/Assets/TrippyCubeScript.cs
@@ -24,6 +24,7 @@ public class TrippyCubeScript : MonoBehaviour
             for (int y = 0; y < height; y++)
             {
                 GameObject cubeClone = (GameObject)Instantiate(movingCube);
+                cubeClone.transform.parent = this.transform;
                 cubeClone.transform.Translate(x * 1.1f, 0f, y * 1.1f);
                 cubeclones[y * width + x] = cubeClone;
             }
@@ -46,9 +47,18 @@ public class TrippyCubeScript : MonoBehaviour
         Debug.Log(alpha);
 
         Vector3 currentPos = personPosition.transform.position;
+        float[] pMid;
 
-        //middle point on the screen
-        float[] pMid = { (float)width / 2.0f, (float)height / 2.0f };
+        {
+            
+            int x = (Mathf.FloorToInt(currentPos.x / 1.1f));
+            int y = (Mathf.FloorToInt(currentPos.z / 1.1f));
+            transform.position = new Vector3(x - width / 2f, 0, y - height / 2f);
+
+            //middle point on the screen
+            pMid = new float[]{ (float)(width / 2.0f - x), (float)(height / 2.0f - y)};
+        }
+        
 
         //this is the screen length squared, from the middle point
         float screenLength = pMid[0] * pMid[0] + pMid[1] * pMid[1];
@@ -66,17 +76,18 @@ public class TrippyCubeScript : MonoBehaviour
         float[] data = new float[width * height];
         trippyBuffer.GetData(data);
         
-        for(int x = 0; x < width; x++) {
+
+        //personPosition.transform.position = new Vector3(currentPos.x, cubePos + 10.4f, currentPos.z);
+
+
+        for (int x = 0; x < width; x++) {
             for(int y = 0; y < height; y++){
                 float cubePos = (float)(5.0 * data[y * width + x] / 255.0);
                 
                 GameObject cube = cubeclones[y * width + x];
                 Vector3 pos = cube.transform.position;
                 cube.transform.position = new Vector3(pos.x, cubePos, pos.z);
-                if (((int)(currentPos.x / 1.1)) == x && ((int)(currentPos.z / 1.1)) == y)
-                {
-                    personPosition.transform.position = new Vector3(currentPos.x, cubePos + 10.4f, currentPos.z);
-                }
+               
             }
         }
     }
